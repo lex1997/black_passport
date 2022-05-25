870fb8248292a91a408df0494a0b459b318a54a5
aleksei7gnevashev@gmail.com <aleksei7gnevashev@gmail.com>
2022-05-26 00:56:53 +0500

    доработка метода обновления баз фмс и рфм

--- passport/views.py
+++ passport/views.py
@@ -1,8 +1,4 @@
-# from rest_framework import generics
-# from django.shortcuts import render
-#
-# from rest_framework.response import Response
-# from rest_framework.views import APIView
+
 
 from django.http import HttpResponse
 from django.views.decorators.csrf import csrf_exempt
