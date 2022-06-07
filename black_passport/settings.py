02d4f8dc3d5632afeda449cce14d67713e29a088
alex7gnev <aleksei7gnevashev@gmail.com>
2022-06-07 21:06:03 +0500

    конечная рихтовка

--- black_passport/settings.py
+++ black_passport/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-^qx6_msrsc0@f0i925p3yu%sq$-y(c^bk4lsj731-#mqe+c+h$'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ["blackpassport.datalab.fit.ru", "localhost"]
 
