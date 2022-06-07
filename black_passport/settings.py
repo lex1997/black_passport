ab132d71e75cc21d61ec4b86c53598c554fbb9ec
alex7gnev <aleksei7gnevashev@gmail.com>
2022-06-07 22:15:47 +0500

    конечная рихтовка

--- black_passport/settings.py
+++ black_passport/settings.py
@@ -82,7 +82,7 @@ DATABASES = {
         "USER": os.getenv("POSTGRES_USER", "rfmfms"),
         "PASSWORD": os.getenv("POSTGRES_PASSWORD", "QWErty!)!)"),
         "HOST": os.getenv("POSTGRES_HOST", "localhost"),
-        "PORT": os.getenv("POSTGRES_PORT", 21997),
+        "PORT": os.getenv("POSTGRES_PORT", 5432),
         "DISABLE_SERVER_SIDE_CURSORS": True,
     }
 }
